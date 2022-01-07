47e990ff53b3bd67127af12cb4c8c5190fdd9551
sayanidhuleshia <53673552+sayanidhuleshia@users.noreply.github.com>
2022-01-07 20:45:48 +1000

    first commit

---
+++ ICDec2021/ICDec2021/Program.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+// See https://aka.ms/new-console-template for more information
+
+//open chrome browser
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+
+//launch turnup portal
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/");
+
+//identify username textbox and enter valid username
+IWebElement userNameTextbox = driver.FindElement(By.Id("UserName"));
+userNameTextbox.SendKeys("hari");
+
+//identify password textbox and enter valid password
+IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
+
+//click on login button
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+loginButton.Click();
+
+//check if user is logged in successfully
+IWebElement helloHari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+if (helloHari.Text == "Hello hari!")
+{
+    Console.WriteLine("Logged in successfully, test passed");
+}
+else
+{
+    Console.WriteLine("Login failed, test failed");
+}
+
+
+
